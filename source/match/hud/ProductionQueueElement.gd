9f038af9565d9ac4e094866a143a1a11591fbe08
King-Darius <111044019+King-Darius@users.noreply.github.com>
2025-10-19 18:41:37 +0100

    Fix production queue repeat handling and format scripts

--- source/match/hud/ProductionQueueElement.gd
+++ source/match/hud/ProductionQueueElement.gd
@@ -7,38 +7,35 @@ var queue_element = null
 
 
 func _ready():
-        if queue == null or queue_element == null:
-                return
-        queue_element.changed.connect(_on_queue_element_changed)
-        pressed.connect(func(): queue.cancel(queue_element))
-        toggle_mode = false
-        var resource_path = queue_element.unit_prototype.resource_path
-        var unit_label = resource_path.get_file().get_basename().replace("_", " ")
-        text = unit_label.capitalize()
-        _update_progress_label()
-        tooltip_text = _format_tooltip(resource_path)
+	if queue == null or queue_element == null:
+		return
+	queue_element.changed.connect(_on_queue_element_changed)
+	pressed.connect(func(): queue.cancel(queue_element))
+	toggle_mode = false
+	var resource_path = queue_element.unit_prototype.resource_path
+	var unit_label = resource_path.get_file().get_basename().replace("_", " ")
+	text = unit_label.capitalize()
+	_update_progress_label()
+	tooltip_text = _format_tooltip(resource_path)
 
 
 func _on_queue_element_changed():
-        _update_progress_label()
+	_update_progress_label()
 
 
 func _update_progress_label():
-        var progress_percent = int(queue_element.progress() * 100.0)
-        find_child("Label").text = "{0}%".format([progress_percent])
+	var progress_percent = int(queue_element.progress() * 100.0)
+	find_child("Label").text = "{0}%".format([progress_percent])
 
 
 func _format_tooltip(resource_path):
-        var cost = Constants.Match.Units.PRODUCTION_COSTS.get(resource_path, {})
-        var time_total = Constants.Match.Units.PRODUCTION_TIMES.get(resource_path, 0.0)
-        var time_string = String.num(time_total, 1)
-        var lines = [
-                text,
-                tr("Cost: %s / %s") % [
-                        cost.get("resource_a", 0),
-                        cost.get("resource_b", 0)
-                ],
-                tr("Build time: %s s") % time_string,
-                tr("Click to cancel this order."),
-        ]
-        return "\n".join(lines)
+	var cost = Constants.Match.Units.PRODUCTION_COSTS.get(resource_path, {})
+	var time_total = Constants.Match.Units.PRODUCTION_TIMES.get(resource_path, 0.0)
+	var time_string = String.num(time_total, 1)
+	var lines = [
+		text,
+		tr("Cost: %s / %s") % [cost.get("resource_a", 0), cost.get("resource_b", 0)],
+		tr("Build time: %s s") % time_string,
+		tr("Click to cancel this order."),
+	]
+	return "\n".join(lines)
--- source/match/units/actions/MovingToUnit.gd
+++ source/match/units/actions/MovingToUnit.gd
@@ -6,40 +6,39 @@ var _target_offset = null
 
 
 func _init(target_unit, target_position_override = null):
-        _target_unit = target_unit
-        if target_position_override != null:
-                _target_position_override = target_position_override * Vector3(1, 0, 1)
+	_target_unit = target_unit
+	if target_position_override != null:
+		_target_position_override = target_position_override * Vector3(1, 0, 1)
 
 
 func _process(_delta):
-        if Utils.Match.Unit.Movement.units_adhere(_unit, _target_unit):
-                queue_free()
+	if Utils.Match.Unit.Movement.units_adhere(_unit, _target_unit):
+		queue_free()
 
 
 func _ready():
-        _target_unit.tree_exited.connect(queue_free)
-        if _target_position_override != null:
-                _target_offset = _target_position_override - _target_unit.global_position_yless
-                if _target_offset.length() < 0.001:
-                        _target_offset = Vector3.RIGHT * (_target_unit.radius + _unit.radius)
-                _target_position = _target_unit.global_position_yless + _target_offset
-        else:
-                var direction = _unit.global_position_yless - _target_unit.global_position_yless
-                if direction.length() < 0.001:
-                        direction = Vector3.RIGHT
-                _target_position = (
-                        _target_unit.global_position_yless
-                        + direction.normalized() * _target_unit.radius
-                )
-        super()
+	_target_unit.tree_exited.connect(queue_free)
+	if _target_position_override != null:
+		_target_offset = _target_position_override - _target_unit.global_position_yless
+		if _target_offset.length() < 0.001:
+			_target_offset = Vector3.RIGHT * (_target_unit.radius + _unit.radius)
+		_target_position = _target_unit.global_position_yless + _target_offset
+	else:
+		var direction = _unit.global_position_yless - _target_unit.global_position_yless
+		if direction.length() < 0.001:
+			direction = Vector3.RIGHT
+		_target_position = (
+			_target_unit.global_position_yless + direction.normalized() * _target_unit.radius
+		)
+	super()
 
 
 func _on_movement_finished():
-        if Utils.Match.Unit.Movement.units_adhere(_unit, _target_unit):
-                queue_free()
-        else:
-                if _target_offset != null:
-                        _target_position = _target_unit.global_position_yless + _target_offset
-                else:
-                        _target_position = _target_unit.global_position
-                _movement_trait.move(_target_position)
+	if Utils.Match.Unit.Movement.units_adhere(_unit, _target_unit):
+		queue_free()
+	else:
+		if _target_offset != null:
+			_target_position = _target_unit.global_position_yless + _target_offset
+		else:
+			_target_position = _target_unit.global_position
+		_movement_trait.move(_target_position)
